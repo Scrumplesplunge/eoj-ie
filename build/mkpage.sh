701b59120b17ce36fae2c9ea045e8599a1cc9bba
Joe Fowler <scrumplesplunge@gmail.com>
2021-01-17 12:57:22 +0000

    Remove some unnecessary quotes and date formatting.

--- build/mkpage.sh
+++ build/mkpage.sh
@@ -17,12 +17,12 @@ KEYWORDS="$(jq -r '.keywords | join(", ")' "$JSON")"
 # Optionally generate "previous" and "next" links.
 if [[ $INDEX -gt 1 ]]; then
   LINK_TITLE="$(jq -r '.title' "$((INDEX - 1))/info.json")"
-  PREVIOUS="<a class=\"previous\" href=\"/$((INDEX - 1))\">← $LINK_TITLE</a>"
+  PREVIOUS="<a class=previous href=\"/$((INDEX - 1))\">← $LINK_TITLE</a>"
 fi
 
 if [[ -d $((INDEX + 1)) ]]; then
   LINK_TITLE="$(jq -r '.title' "$((INDEX + 1))/info.json")"
-  NEXT="<a class=\"next\" href=\"/$((INDEX + 1))\">$LINK_TITLE →</a>"
+  NEXT="<a class=next href=\"/$((INDEX + 1))\">$LINK_TITLE →</a>"
 fi
 
 pretty_date() {
@@ -39,20 +39,20 @@ pretty_date() {
 # Generate the page.
 cat <<EOF
 <!doctype html>
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta name="created" content="$(date -d "$DATE" -Iseconds)">
-<meta name="keywords" content="$KEYWORDS">
-<link id="theme" rel="stylesheet" href="/themes/default.css">
+<meta charset=utf-8>
+<meta name=viewport content="width=device-width, initial-scale=1">
+<meta name=created content="$DATE">
+<meta name=keywords content="$KEYWORDS">
+<link id=theme rel=stylesheet href=/themes/default.css>
 <script>
 // Take a look in /themes if you want to pick a different one.
 document.getElementById("theme").href =
     localStorage.theme || "/themes/default.css";
 </script>
 <title>$TITLE</title>
-<header id="header">
+<header id=header>
 <h1>$TITLE</h1>
-<time datetime="$(date -d "$DATE" -Iseconds)">$(pretty_date "$DATE")</time>
+<time datetime="$DATE">$(pretty_date "$DATE")</time>
 <p>
 $PREVIOUS
 $NEXT
